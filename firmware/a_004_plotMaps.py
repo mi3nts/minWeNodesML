c0ffa340fe9c56cbb151b25e5aeae4e4c5f61567
lakihaomal <lhw150030@utdallas.edu>
2023-09-28 12:09:49 -0500

    plots added

--- firmware/a_004_plotMaps.py
+++ firmware/a_004_plotMaps.py
@@ -38,7 +38,7 @@ endTime       = mintsDefinitions['endTime']
 start_time_df = datetime.strptime(mintsDefinitions['startTime'], '%Y_%m_%d_%H_%M_%S')
 end_time_df   = datetime.strptime(mintsDefinitions['endTime'], '%Y_%m_%d_%H_%M_%S')
 
-varUsed   = mintsDefinitions['varUsed']
+# varUsed   = mintsDefinitions['varUsed']
 
 
 # Create an empty DataFrame to store the merged data
@@ -51,10 +51,9 @@ for currentNode in nodeIDs:
     nodeName    = currentNode['nodeName']
     print("========================NODES========================")
 
-    print("Reading node data for "+ nodeName+ " with node ID " + nodeID)
-    filePathePrePkl = dataFolderParent + "/mergedPickles/" + nodeID +  "/" +nodeID + "_" +nodeName + "_" +startTime +"_" +endTime 
+    # print("Reading node data for "+ nodeName+ " with node ID " + nodeID)
+    filePathePrePkl = dataFolderParent + "/mergedPickles/" + nodeID +  "/" +nodeID + "_" +nodeName + "_" +startTime +"-" +endTime 
     file_path       = filePathePrePkl    + '.pkl'
-    # file_path       = dataFolderParent + "/mergedPickles/" + nodeID +  "/" +nodeID + "_" +startTime +"-" +endTime + '_resampled_'+resampleTime+'.pkl'
     directory_path = os.path.dirname(file_path)
 
     if os.path.exists(file_path):
@@ -62,49 +61,62 @@ for currentNode in nodeIDs:
         with open(file_path, 'rb') as file:
             df_mints = pickle.load(file)
 
-        color_column = varUsed
+        print("TEST 1")
         map_center = [32.967407, -96.703391]
-        m = folium.Map(location=map_center, zoom_start=20)
+        # m = folium.Map(location=map_center, zoom_start=20)
 
-        color_scale = folium.LinearColormap(
+        # print(mintsDefinitions['vars'])
+        # Create a feature group for the markers
+        print("TEST 2")
+        
+        for rowVar in mintsDefinitions['vars']:
+            m = folium.Map(location=map_center, zoom_start=20)
+            print("TEST 3")
+        
+            varUsed = rowVar['ID']
+            varLabel = rowVar['Label']
+            print("========================NODES========================")
+
+            print("Reading node data for "+ nodeName+ " with node ID " + nodeID + " for " + varUsed + " "+ varLabel)
+            print("TEST 4")
+            marker_group = folium.FeatureGroup(name= varLabel)
+            color_column = varUsed
+            color_scale = folium.LinearColormap(
                         colors=['yellow', 'red'],
                         vmin=df_mints[color_column].min(),  # Minimum value from the specified column
                         vmax=df_mints[color_column].max()   # Maximum value from the specified column
                         )
-        
-        # Create a feature group for the markers
-        marker_group = folium.FeatureGroup(name= mintsDefinitions['varLabel'])
-        
-        for index, row in df_mints.iterrows():
-            latitude, longitude, varUsedNow = row['latitudeCoordinate_GPSGPGGA2'], row['longitudeCoordinate_GPSGPGGA2'], row[varUsed]
-            color = color_scale(varUsedNow)
+            for index, row in df_mints.iterrows():
+                latitude, longitude, varUsedNow = row['latitudeCoordinate_GPSGPGGA2'], row['longitudeCoordinate_GPSGPGGA2'], row[varUsed]
+                color = color_scale(varUsedNow)
+                
+                folium.CircleMarker(
+                    location=(latitude, longitude),
+                    radius=5,
+                    color=color,
+                    fill=True,
+                    fill_color=color,
+                    fill_opacity=0.7,
+                    popup= varLabel + f':{varUsedNow}'
+                ).add_to(marker_group)
+
+
+            marker_group.add_to(m)
             
-            folium.CircleMarker(
-                location=(latitude, longitude),
-                radius=5,
-                color=color,
-                fill=True,
-                fill_color=color,
-                fill_opacity=0.7,
-                popup=f' PM 2.5: {varUsedNow}'
-            ).add_to(marker_group)
+            # Add a color legend
+            color_scale.add_to(m)
 
+            folium.LayerControl().add_to(m)
 
-        marker_group.add_to(m)
-        
-        # Add a color legend
-        color_scale.add_to(m)
+            file_path = dataFolderParent + "/plots/surveplots/" + nodeID +  "/" +nodeID + "_" + nodeName + "_" + varUsed + "_"+ startTime +"-" +endTime + '_surveyPlot_' +resampleTime+'.html'
+            directory_path = os.path.dirname(file_path)
 
-        folium.LayerControl().add_to(m)
+            # Create the directory if it doesn't exist
+            if not os.path.exists(directory_path):
+                os.makedirs(directory_path)
 
-        file_path = dataFolderParent + "/plots/surveplots/" + nodeID +  "/" +nodeID + "_" + varUsed + "_"+ startTime +"-" +endTime + '_surveyPlot_' +resampleTime+'.html'
-
-        directory_path = os.path.dirname(file_path)
-
-        # Create the directory if it doesn't exist
-        if not os.path.exists(directory_path):
-            os.makedirs(directory_path)
-
-        # Save the map to an HTML file
-        # m.save(dataFolderParent + "/plots/surveplots/" + nodeID +  "/" +nodeID + "_" + varUsed + "_" +startTime +"-" +endTime + '_surveyPlot_'+resampleTime+'.html')
-        m.save(file_path)
+            # Save the map to an HTML file
+            # m.save(dataFolderParent + "/plots/surveplots/" + nodeID +  "/" +nodeID + "_" + varUsed + "_" +startTime +"-" +endTime + '_surveyPlot_'+resampleTime+'.html')
+            print(file_path)
+            print("------")
+            m.save(file_path)
